d173cd7a535c186998e4efa6aa4aaa7856941841
sandeep.bardewa <sandeep.bardewa@PUNECPU536.EIC.EINFOCHIPS.COM>
2019-09-16 15:51:17 +0530

    New changes in controller

--- src/main/java/com/arlo/controller/PaymentController.java
+++ src/main/java/com/arlo/controller/PaymentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
 
 	
-	private static final String VAL = "dockerization with jenkins and building images";
+	private static final String VAL = "dockerization with jenkins and building images and running";
 
 	@RequestMapping("/docker")
 	public String dockerGet() {
